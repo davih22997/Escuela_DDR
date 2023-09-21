acdc03ac15f7132a09740820dff63654afa0a41e
davih22997 <davih22997@gmail.com>
2023-09-21 12:31:29 +0200

    feat: Functions with default parameters defined
    New part of the project created:
    - 06-funciones.js script created

---
+++ master-javatypescript/javascript-moderno/scripts/06-funciones.js
@@ -0,0 +1,9 @@
+// Parámetros por defecto
+function saludo(persona = "Víctor Robles", year=2022) {
+
+    console.log("Hola, te mando un saludo, " + persona + " estamos a " + year); 
+}
+
+saludo("Pepe", 2023);
+
+// Funciones flecha
