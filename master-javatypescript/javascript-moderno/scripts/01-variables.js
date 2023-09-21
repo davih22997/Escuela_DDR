3dcc97e2c3b0c4882690727566b06a9b2ae381bb
davih22997 <davih22997@gmail.com>
2023-09-21 09:29:37 +0200

    feat: Introducing variables in JS
    - VAR variables -> Global
    - LET variables -> Block
    - CONST variables -> Constants

--- master-javatypescript/javascript-moderno/scripts/01-variables.js
+++ master-javatypescript/javascript-moderno/scripts/01-variables.js
@@ -15,3 +15,44 @@ function mostrarNombre() {
 
 mostrarNombre();
 console.log("Fuera:", nombre);
+
+// LET: ámbito de bloque
+let pais = "España"
+
+console.log(pais);
+
+function mostrarPais() {
+    console.log(pais);
+}
+
+// mostrarPais();
+
+if(pais == "España") {
+    let continente = "Europa";
+    console.log(continente);
+}
+
+// No accesible desde fuera del bloque
+// console.log('fuera', continente);
+
+for (let contador = 0; contador <= 5; contador++) {
+    console.log(contador);
+}
+
+// No funciona con let (si no pones nada o pones var, sí)
+// console.log("CONTADOR:", contador);
+
+// CONST: Crear constantes o variables que su contenido
+// no es variable
+
+const edad = 18;
+console.log(edad);
+
+// No se puede cambiar
+// edad = 12;
+console.log(edad);
+
+const ciudad = "Madrid";
+const fechaNacimiento = 1978;
+
+console.log(ciudad, fechaNacimiento);
