4486dbf30de435ad88f33b0b1990448cc7bae655
davih22997 <davih22997@gmail.com>
2023-09-21 18:04:09 +0200

    feat: Functions and getters & setters defined to class coche
    -> All settings in 17-classes.js file

--- master-javatypescript/javascript-moderno/scripts/17-clases.js
+++ master-javatypescript/javascript-moderno/scripts/17-clases.js
@@ -18,13 +18,41 @@ class Coche{
     }
     
     // Métodos / acciones o funcionalidades del objeto
+    subirVelocidad() {
+        this.velocidad++;
+    }
+
+    bajarVelocidad() {
+        this.velocidad--;
+    }
+
+    // Métodos SET y GET (setter y getters)
+    set guardarMarca(marca){
+        this.marca = marca;
+    }
+
+    get devolverMarca(){
+        return this.marca;
+    }
 
 }
 
 // Crear objeto
-let mi_coche = new Coche({});
-console.log(mi_coche.marca, mi_coche.modelo, mi_coche.year);
+let mi_coche = new Coche({marca:"Audi", modelo:"C4", color: "Rojo", velocidad: 100, year: 2023});
+console.log(mi_coche.marca, mi_coche.modelo, mi_coche.velocidad + "km/h");
 
-let segundo_coche = new Coche({ modelo: "Clio",marca: "Renault", year: 2013});
+mi_coche.subirVelocidad();
+mi_coche.subirVelocidad();
+mi_coche.subirVelocidad();
+mi_coche.subirVelocidad();
+mi_coche.subirVelocidad();
+mi_coche.bajarVelocidad();
+console.log(mi_coche.velocidad + "km/h");
+mi_coche.guardarMarca = "Toyota";
+
+console.log(mi_coche.devolverMarca);
+
+
+let segundo_coche = new Coche({modelo: "Clio", marca: "Renault", year: 2013});
 console.log(segundo_coche.marca, segundo_coche.modelo, segundo_coche.year);
 
