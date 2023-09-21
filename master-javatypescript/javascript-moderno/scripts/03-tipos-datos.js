bd8a12551189c326e00307d823d9667b320d1c02
davih22997 <davih22997@gmail.com>
2023-09-21 11:11:33 +0200

    feat: Diferent data created
    -> All in 03-tipos-datos.js file

---
+++ master-javatypescript/javascript-moderno/scripts/03-tipos-datos.js
@@ -0,0 +1,33 @@
+// String
+let nombre = "Víctor Robles"
+console.log(typeof(nombre));
+
+// Number
+let numero = 14;
+let decimal = 3.2;
+console.log(typeof(numero));
+console.log(typeof(decimal));
+
+// Boolean
+let mayor_edad = true;
+let menor_edad = Boolean(0); // 1 es true, 0 es false
+
+console.log(typeof(mayor_edad));
+console.log(typeof(menor_edad));
+
+// Array
+let paises = ['España', 'México', 'Colombia', 'Argentina'];
+console.log(typeof(paises));
+console.log(typeof(paises[1]));
+
+// Null
+let vacio;
+console.log(typeof(vacio));
+
+// Undefined
+let no_definido = undefined;
+console.log(typeof(no_definido));
+
+// JSON - JavaScript Object Notation - Objectos literales
+
+
