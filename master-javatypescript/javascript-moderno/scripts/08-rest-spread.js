bb977023cc37b9fa88cdb3ba317f273c2317ff3d
davih22997 <davih22997@gmail.com>
2023-09-21 13:32:38 +0200

    feat: Rest example defined
    -> All in 08-rest-spread.js

--- master-javatypescript/javascript-moderno/scripts/08-rest-spread.js
+++ master-javatypescript/javascript-moderno/scripts/08-rest-spread.js
@@ -20,3 +20,18 @@ let superheroes = ["Flash", "Catwoman", ...heroes];
 console.log(superheroes);
 
 // REST
+function peliculas(pelicula1, pelicula2, ...restoDePeliculas) {
+    console.log(pelicula1);
+    console.log(pelicula2);
+    for (pelicula of restoDePeliculas)
+        console.log(pelicula)
+    
+}
+
+peliculas("El Señor de los anillos", 
+"Fast and furious",
+"Batman VS Superman",
+"Gran Torino", 
+"Sin límites"
+);
+
