d715efc8943c54c86f8200ecb89880db0f281d59
davih22997 <davih22997@gmail.com>
2023-09-21 12:22:37 +0200

    feat: Defined some iterables & iterators
    -> All in 05-bucles.js script

--- master-javatypescript/javascript-moderno/scripts/05-bucles.js
+++ master-javatypescript/javascript-moderno/scripts/05-bucles.js
@@ -18,7 +18,7 @@ for(let nombre of nombres) {
 // For in -> Accede al índice
 //          -> también recorre json
 for(let indice in nombres) {
-    console.log(indice, nombres[indice]);
+    // console.log(indice, nombres[indice]);
 }
 
 // For Each
@@ -27,5 +27,57 @@ nombres.forEach(function(nombre_actual, indice, arreglo) {
     console.log(indice, nombre_actual, arreglo);
 });
 */
-nombres.forEach((nombre_actual, indice, arreglo) => console.log(indice, nombre_actual, arreglo));
+// nombres.forEach((nombre_actual, indice, arreglo) => console.log(indice, nombre_actual, arreglo));
+
+// Recorrer string
+let sitioweb = "victorroblesweb.es";
+
+for (let letra of sitioweb) {
+    //console.log(letra);
+}
+
+// Iterables
+const mi_iterable = nombres[Symbol.iterator]();
+// console.log(typeof mi_iterable, mi_iterable);
+console.log(mi_iterable.next());
+console.log(mi_iterable.next());
+console.log(mi_iterable.next());
+console.log(mi_iterable.next());
+
+// JSON
+let fruta = {
+    nombre: "Manzana",
+    color: "Verde",
+    temporada: "Invierno"
+}
+
+fruta[Symbol.iterator] = function() {
+    let indice = 0;
+    let valores = Object.values(this);
+
+    return {
+        next() {
+            if(indice >= valores.length) {
+                return {
+                    done: true,
+                    value:undefined
+                };
+            }
+
+            return {
+                done: false,
+                value: valores[indice++]
+            }
+
+        }
+    };
+}
+
+for(let propiedad of fruta) {
+    //console.log(propiedad);
+}
+
+for(let propiedad in fruta) {
+    console.log(propiedad, fruta[propiedad]);
+}
 
