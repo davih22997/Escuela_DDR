2b387ec6b8e7ea88f5f4285b6592e37e90b4a383
davih22997 <davih22997@gmail.com>
2023-09-25 11:04:19 +0200

    feat: JavaScript project created
    - Some files created:
    -> index.html file
    -> fecha.ts & main.ts scripts
    --> Compiled into .js scripts

---
+++ master-javatypescript/typescript-moderno/main.ts
@@ -0,0 +1,6 @@
+let nombre: string = "Víctor";
+let web: string = "victorroblesweb.es";
+
+console.log("El nombre es:", nombre, web);
+console.log("El nombre es:", nombre, web);
+console.log("El nombre es:", nombre, web);
