6d059a1b26921200a8c03597aa1e01b95e8cd976
davih22997 <davih22997@gmail.com>
2023-09-25 11:31:46 +0200

    feat: Basic types examples defined
    - 01-tipos-basicos.ts script created with all examples
    -> 01-tipos-basicos.js generated automatically

---
+++ master-javatypescript/typescript-moderno/scripts/01-tipos-basicos.js
@@ -0,0 +1,12 @@
+"use strict";
+// Tipos de dato básicos
+let texto = "Hola que tal!!";
+let numero = 10;
+numero = 12.4;
+numero = 11.1;
+let verdadero = true;
+verdadero = false;
+let normal = "cualquier cosa";
+normal = true;
+normal = 14;
+console.log(normal);
---
+++ master-javatypescript/typescript-moderno/src/01-tipos-basicos.ts
@@ -0,0 +1,16 @@
+// Tipos de dato básicos
+let texto:string = "Hola que tal!!";
+
+let numero:number = 10;
+numero = 12.4;
+numero = 11.1;
+
+let verdadero:boolean = true;
+verdadero = false;
+
+let normal:any = "cualquier cosa";
+normal = true;
+normal = 14;
+
+console.log(normal);
+
