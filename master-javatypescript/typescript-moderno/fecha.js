a36c96cf52ca4682ebe1673be44058b49aa36258
davih22997 <davih22997@gmail.com>
2023-09-25 11:19:41 +0200

    feat: All settings of the project configured
    -> New folders:
    - scripts: Contains all .js scripts
    - src: Contains all .ts scripts
    -> Settings in tsconfig.json

--- master-javatypescript/typescript-moderno/fecha.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-console.log("Fecha actual:");
-let fecha = new Date();
-// console.log(fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear());
-console.log(fecha.getDate());
--- master-javatypescript/typescript-moderno/src/fecha.ts
+++ master-javatypescript/typescript-moderno/src/fecha.ts
@@ -1,5 +1,5 @@
 "use strict";
-console.log("Fecha actual:");
+console.log("Fecha actual: hola");
 let fecha = new Date();
 // console.log(fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear());
 console.log(fecha.getDate());
