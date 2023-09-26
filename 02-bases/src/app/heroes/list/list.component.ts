3b0c54db97f6afc7988bfb3e0942bd564ac25c18
davih22997 <davih22997@gmail.com>
2023-09-26 18:13:10 +0200

    feat: *ngFor directive created
    -> Some content created in list.component.ts
    -> Some content created in list.component.html
    -> *ngFor is used in the .html file to get all elements of heroNames list created in the .ts file

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
-
+  public heroNames: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'She Hulk',
+    'Thor'
+  ]
 }
