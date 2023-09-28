fe12ff3081c273a9127ae982a3c2d5c8bec5f445
davih22997 <davih22997@gmail.com>
2023-09-28 10:19:19 +0200

    feat: Imported http and some parameters configured to the project

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 // const GIPHY_API_KEY = "4XvatTfT1gmoROr2wm8i68pLTJnyE6D3";
@@ -7,8 +8,9 @@ export class GifsService {
 
   private _tagsHistory: string[] = [];
   private _apiKey: string = "4XvatTfT1gmoROr2wm8i68pLTJnyE6D3";
+  private _serviceUrl: string = "https://api.giphy.com/v1/gifs";
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -24,11 +26,27 @@ export class GifsService {
       this._tagsHistory = this._tagsHistory.splice(0,10);
   }
 
+  // async searchTag(tag: string):Promise<void> {
   searchTag(tag: string):void {
-    if (tag.length === 0)
-      return;
-
+    if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this._apiKey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get(`${this._serviceUrl}/search`, {params: params})
+      .subscribe(resp => {
+        console.log(resp);
+      });
+
+
+
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=4XvatTfT1gmoROr2wm8i68pLTJnyE6D3&q=valorant&limit=10')
+    //   .then(resp => resp.json())
+    //   .then(data => console.log(data));
+
   }
 
 }
