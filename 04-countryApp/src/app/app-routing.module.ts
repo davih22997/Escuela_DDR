1b31b3a3c5fcf40c2c10ced6917578f3a854e78a
davih22997 <davih22997@gmail.com>
2023-09-28 16:27:29 +0200

    feat: Countries Routing Module created

--- 04-countryApp/src/app/app-routing.module.ts
+++ 04-countryApp/src/app/app-routing.module.ts
@@ -1,21 +1,21 @@
-import { NgModule, Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
-  path: '',
-  component: HomePageComponent
+    path: '',
+    component: HomePageComponent
   },
   {
     path: 'about',
     component: AboutPageComponent
   },
   {
-    path: 'contact',
+    path: 'countries',
     component: ContactPageComponent
   },
   {
