b498fdb4bfaa8b6a0d1e29aa2f8e86bf749d8578
davih22997 <davih22997@gmail.com>
2023-09-28 11:23:21 +0200

    feat: Read of persistent data implemented

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -13,7 +13,15 @@ export class GifsService {
   private _apiKey: string = "4XvatTfT1gmoROr2wm8i68pLTJnyE6D3";
   private _serviceUrl: string = "https://api.giphy.com/v1/gifs";
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs service loaded')
+
+    if (this._tagsHistory.length === 0) return;
+
+    this.searchTag(this._tagsHistory[0]);
+
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -32,6 +40,12 @@ export class GifsService {
 
   private saveLocalStorage():void {
     localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage():void {
+    if (! localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
   }
 
   // async searchTag(tag: string):Promise<void> {
