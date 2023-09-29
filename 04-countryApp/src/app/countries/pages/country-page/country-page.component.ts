f585b852b1dc088262e236247e28a79d9c41d5fe
davih22997 <davih22997@gmail.com>
2023-09-29 10:30:04 +0200

    feat: Search by code finalized
    -> It has a good format in a table
    -> Translations added

--- 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
+++ 04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap, tap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -10,21 +12,34 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class CountryPageComponent implements OnInit{
 
+  public country?: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private countriesService: CountriesService,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params
-      .subscribe(({id}) => {
+      .pipe(
+        switchMap(({id}) =>
+        this.countriesService.searchCountryByAlphaCode(id) ),
+      )
+      .subscribe(country => {
+        if (!country)
+          return this.router.navigateByUrl('');
 
-        this.countriesService.searchCountryByAlphaCode(id)
-          .subscribe(country => {
-            console.log({ country })
-          });
-
+        return this.country = country;
+        // return;
       });
+  }
+
+  searchCountry(code: string) {
+    this.countriesService.searchCountryByAlphaCode(code)
+    .subscribe(country => {
+      console.log({ country })
+    });
   }
 
 
