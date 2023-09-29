7914cd554c5a6b4e447375b38ba4c99b17aa5b47
davih22997 <davih22997@gmail.com>
2023-09-29 12:06:20 +0200

    feat: OnDestroy subscriptions implemented

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -19,6 +19,7 @@ export class CountriesService {
   }
 
   searchCountryByAlphaCode (code: string):Observable<Country | null> {
+
     const url= `${this.apiUrl}/alpha/${code}`;
 
     return this.http.get<Country[]>(url)
--- 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
+++ 04-countryApp/src/app/shared/components/search-box/search-box.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
-import { Subject, debounceTime } from 'rxjs';
+import { Component, EventEmitter, Input, Output, OnInit, OnDestroy } from '@angular/core';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -7,9 +7,10 @@ import { Subject, debounceTime } from 'rxjs';
   styles: [
   ]
 })
-export class SearchBoxComponent implements OnInit{
+export class SearchBoxComponent implements OnInit, OnDestroy {
 
   private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
 
   @Input()
   public placeholder:string = '';
@@ -21,13 +22,17 @@ export class SearchBoxComponent implements OnInit{
   public onDebounce = new EventEmitter<string>();
 
   ngOnInit(): void {
-    this.debouncer
-      .pipe(
-        debounceTime(300)
+    this.debouncerSubscription = this.debouncer
+    .pipe(
+      debounceTime(300)
       )
       .subscribe(value => {
         this.onDebounce.emit(value);
       });
+    }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
   }
 
   emitValue(value:string):void {
