1f45dd7e42fa370de078bf236da21c50f6d03790
davih22997 <davih22997@gmail.com>
2023-09-29 13:46:53 +0200

    feat: Persistence of data - Completed
    - All searching data is stored while you are surfing the web

--- 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/region.type';
@@ -11,21 +11,32 @@ import { Country } from '../../interfaces/country.interface';
   styles: [
   ]
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
   public countries: Country[] = [];
   public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
   public selectedRegion?: Region;
+  public isLoading: boolean = false;
 
   constructor(private countriesService: CountriesService) { }
 
+  ngOnInit(): void {
+    this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+
+    if (this.selectedRegion)
+      this.searchByRegion(this.selectedRegion);
+  }
+
   searchByRegion(region: Region):void {
+
+    this.isLoading = true;
 
     this.selectedRegion = region;
 
     this.countriesService.searchRegion(region)
       .subscribe(countries => {
         this.countries = countries;
+        this.isLoading = false;
       });
-   }
+  }
 
 }
