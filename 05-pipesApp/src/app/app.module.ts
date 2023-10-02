d822e10d0c5690caebd57bb980d07193ae68b6f7
davih22997 <davih22997@gmail.com>
2023-10-02 13:40:41 +0200

    feat: Date Pipe - More languages

--- 05-pipesApp/src/app/app.module.ts
+++ 05-pipesApp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
@@ -6,6 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
+
+// Configuración del locale de la app
+import localeEs from '@angular/common/locales/es';
+import localeFrCA from '@angular/common/locales/fr-CA';
+// import localeEnUS from '@angular/common/locales/en';
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs);
+registerLocaleData(localeFrCA);
+// registerLocaleData(localeEnUS);
 
 @NgModule({
   declarations: [
@@ -18,7 +29,12 @@ import { SharedModule } from './shared/shared.module';
 
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
