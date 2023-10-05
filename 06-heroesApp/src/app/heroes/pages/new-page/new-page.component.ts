c3d44aaf6275bff5c05be7e69019a84f7df79b7f
davih22997 <davih22997@gmail.com>
2023-10-05 16:35:39 +0200

    feat: CRUD heroes finalized
    -> Now deletion is implemented

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -3,8 +3,10 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-new-page',
@@ -39,7 +41,8 @@ export class NewPageComponent implements OnInit {
     private heroesService: HeroesService,
     private activatedRoute:ActivatedRoute,
     private router:Router,
-    private snackbar:MatSnackBar
+    private snackbar:MatSnackBar,
+    private dialog:MatDialog
   ) {}
 
 
@@ -84,6 +87,24 @@ export class NewPageComponent implements OnInit {
       });
 
 
+  }
+
+  onDeleteHero() {
+    if (!this.currentHero.id) throw Error('Hero id is required');
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value,
+    });
+
+    dialogRef.afterClosed()
+      .pipe(
+        filter((result:boolean) => result),
+        switchMap(() => this.heroesService.deleteHeroById(this.currentHero.id)),
+        filter((wasDeleted:boolean) => wasDeleted)
+      )
+      .subscribe(result => {
+        this.router.navigate(['/heroes']);
+      });
   }
 
   showSnackbar(message:string):void {
