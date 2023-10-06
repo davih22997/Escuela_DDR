91b45b4198b145f85486fb8ae2bc1f9e4d41dc1e
davih22997 <davih22997@gmail.com>
2023-10-06 11:37:44 +0200

    feat: Heroes - login & logout - First steps

---
+++ 06-heroesApp/src/app/auth/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environments } from 'src/environments/environtments';
+import { User } from '../interfaces/user.interface';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  private baseUrl = environments.baseUrl;
+  private user?: User;
+
+  constructor(private http: HttpClient) { }
+
+  get currentUser(): User | undefined {
+    if (!this.user) return undefined;
+
+    return structuredClone(this.user);
+  }
+
+  login(email: string, password: string):Observable<User> {
+
+    // http.post('login', {email, password});
+
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap(user => this.user = user),
+        tap(user => localStorage.setItem('token', 'asdfasdfaA.asa.asdfas')),
+      );
+
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
+
+
+}
--- 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-layout-page',
@@ -26,5 +29,18 @@ export class LayoutPageComponent {
     },
   ]
 
+  constructor(
+    private authService:AuthService,
+    private router: Router
+  ) {}
+
+  get user():User | undefined {
+    return this.authService.currentUser;
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+  }
 
 }
