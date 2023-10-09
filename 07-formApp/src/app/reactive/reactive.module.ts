402d541c400a016964fc0dfd54a0f3882423e462
davih22997 <davih22997@gmail.com>
2023-10-09 12:29:13 +0200

    feat: Reactive forms - Forms validators: First steps
    Validators added to Reactive - Basic Page

--- 07-formApp/src/app/reactive/reactive.module.ts
+++ 07-formApp/src/app/reactive/reactive.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { ReactiveRoutingModule } from './reactive-routing.module';
+
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
@@ -9,14 +10,15 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
 
 @NgModule({
   declarations: [
-  
+
     BasicPageComponent,
-       DynamicPageComponent,
-       SwitchesPageComponent
+    DynamicPageComponent,
+    SwitchesPageComponent
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class ReactiveModule { }
