f658c95a50c00c68503396adbe453a09e474ef45
davih22997 <davih22997@gmail.com>
2023-10-09 16:37:25 +0200

    feat: Validations - Email validations & patterns
    validators.ts file created

--- 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import * as customValidators from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -9,9 +10,9 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required]],
+    name: ['', [Validators.required,  Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
+    username: ['', [Validators.required, customValidators.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]]
   });
