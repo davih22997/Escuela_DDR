99f3679f94ac1face5b96215ae636b4cc1a36027
davih22997 <davih22997@gmail.com>
2023-10-10 12:21:33 +0200

    feat: Reactive-Multiple forms - Country list cleaned when first selector changes

--- 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
+++ 08-selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -15,9 +15,9 @@ export class SelectorPageComponent implements OnInit {
   public countriesByRegion: SmallCountry[] = []
 
   public myForm: FormGroup = this.fb.group({
-    region: ['', Validators.required],
-    country: ['', Validators.required],
-    borders: ['', Validators.required],
+    region:   ['', Validators.required],
+    country:  ['', Validators.required],
+    borders:  ['', Validators.required],
   })
 
   constructor(
@@ -36,10 +36,10 @@ export class SelectorPageComponent implements OnInit {
   onRegionChanged(): void {
     this.myForm.get('region')!.valueChanges
       .pipe(
+        tap( () => this.myForm.get('country')!.setValue('') ),
         switchMap(region => this.countriesService.getCountriesByRegion(region))
       )
       .subscribe(countries => {
-        console.log(countries);
         this.countriesByRegion = countries;
       });
   }
