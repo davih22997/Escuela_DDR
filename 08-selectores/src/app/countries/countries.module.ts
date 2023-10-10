64c570eb312dd06aad3ee6a1ea2b65abb712e408
davih22997 <davih22997@gmail.com>
2023-10-10 11:06:07 +0200

    feat: Reactive - Multiple Forms - First selector

--- 08-selectores/src/app/countries/countries.module.ts
+++ 08-selectores/src/app/countries/countries.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 import { CountriesRoutingModule } from './countries-routing.module';
 
@@ -11,7 +14,8 @@ import { CountriesRoutingModule } from './countries-routing.module';
   ],
   imports: [
     CommonModule,
-    CountriesRoutingModule
+    CountriesRoutingModule,
+    ReactiveFormsModule,
   ]
 })
 export class CountriesModule { }
