e34bdb0bc31fa9165efada1b405dc282b466bc11
davih22997 <davih22997@gmail.com>
2023-10-13 14:14:57 +0200

    feat: Maps in Angular - Map is showed in fullscreen

--- 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiZGF2aWQtZGRyIiwiYSI6ImNsbm9qMGUzZzBqbG8ycW9jcDNneDAxNXYifQ.BWQzu_PRM_psVHDrDbXtHg';
+
 
 @Component({
   templateUrl: './full-screen-page.component.html',
   styleUrls: ['./full-screen-page.component.css']
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+
+ngAfterViewInit() {
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+    });
+}
+
+
 
 }
