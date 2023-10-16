a73fe1d2def9e08e95d8cd924c9a5f3318053baa
davih22997 <davih22997@gmail.com>
2023-10-16 18:27:27 +0200

    feat: Signals in Angular - Project completed

--- 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, computed, effect, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 
 @Component({
@@ -7,7 +7,9 @@ import { User } from '../../interfaces/user-request.interface';
   styles: [
   ]
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnDestroy, OnInit{
+
+  public counter = signal(10);
 
   public user = signal<User>({
     "id": 4,
@@ -19,6 +21,28 @@ export class PropertiesPageComponent {
 
   public fullName = computed(() => `${this.user().first_name} ${this.user().last_name}`)
 
+  public userChangeEffect = effect(() => {
+    console.log(`${this.user().first_name} - ${this.counter()}`);
+
+  });
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.counter.update(current => current + 1);
+
+      if (this.counter() == 15)
+        this.userChangeEffect.destroy();
+
+    }, 1000)
+  }
+
+  ngOnDestroy(): void {
+    // this.userChangeEffect.destroy();
+  }
+
+  increaseBy(value: number) {
+    this.counter.update(current => current + value);
+  }
 
   onFieldUpdated(field: string, value: string) {
 
