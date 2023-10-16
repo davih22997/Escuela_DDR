0f648793df5a4b0e6cb68d8d45b522c16c014cec
davih22997 <davih22997@gmail.com>
2023-10-16 16:50:03 +0200

    feat: Signals in Angular - Counter Signal implemented

--- 11-directiveSignals/src/app/signals/pages/counter-page/counter-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
 
 @Component({
   selector: 'app-counter-page',
   templateUrl: './counter-page.component.html',
-  styles: [
-  ]
 })
 export class CounterPageComponent {
+
+  public counter = signal(10);
+  public squareCounter = computed( () => this.counter() * this.counter() );
+
+  increaseBy(value: number) {
+    // this.counter.set(this.counter() + value);
+    this.counter.update(current => current + value);
+  }
 
 }
