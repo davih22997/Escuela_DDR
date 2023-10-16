77e71dc1ac1d501cc841c3d58c8d771c1bc228d2
davih22997 <davih22997@gmail.com>
2023-10-16 13:57:19 +0200

    feat: Custom policies - directive input
    -> Change of text color implemented

--- 11-directiveSignals/src/app/shared/directives/custom-label.directive.ts
+++ 11-directiveSignals/src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]'
@@ -6,15 +6,28 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit{
 
   private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+
+  @Input() set color(value:string) {
+    this._color = value;
+    this.setStyle();
+  }
 
   constructor( private el: ElementRef<HTMLElement>) {
     // console.log('Constructor de la directiva')
-    console.log(el);
+    // console.log(el);
     this.htmlElement = el;
   }
 
   ngOnInit(): void {
     console.log('Directiva - NgOnInit')
+
+    this.setStyle();
+  }
+
+  setStyle():void {
+    if (! this.htmlElement) return;
+    this.htmlElement!.nativeElement.style.color = this._color;
   }
 
 }
