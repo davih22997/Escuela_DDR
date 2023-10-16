e9ff22b3ca7f3c83ce4b8b967ba3623305004719
davih22997 <davih22997@gmail.com>
2023-10-16 17:59:05 +0200

    feat: Signals in Angular - Screen Structure

--- 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
+  onFieldUpdated(field: string, value: string) {
+
+    console.log({field, value})
+  }
+
 }
