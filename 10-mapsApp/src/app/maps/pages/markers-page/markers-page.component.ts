a42be954034558a684a1584b1ed1fa2e56d22098
davih22997 <davih22997@gmail.com>
2023-10-16 10:25:42 +0200

    feat: Maps in Angular - FlyTo marker implemented

--- 10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -78,4 +78,11 @@ export class MarkersPageComponent {
     this.markers.splice(index,1);
   }
 
+  flyTo(marker: Marker) {
+    this.map?.flyTo({
+      zoom:14,
+      center: marker.getLngLat(),
+    })
+  }
+
 }
