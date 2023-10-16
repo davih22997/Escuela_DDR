b9749359dfbe1936b213c1883c9aa7ca2158300d
davih22997 <davih22997@gmail.com>
2023-10-16 10:00:40 +0200

    feat: Maps in Angular - Markers -> First steps

--- 10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Map, LngLat, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
@@ -6,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class MarkersPageComponent {
 
+  @ViewChild('map')
+  divMap?: ElementRef;
+
+  public map?: Map;
+  // public currentLngLat: LngLat = new LngLat(-74.5, 40)
+  public currentLngLat: LngLat = new LngLat(-66.90898892333702, 10.528248850305616)
+
+  ngAfterViewInit() {
+
+    if (!this.divMap) throw "El elemento HTML no fue encontrado";
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 13, // starting zoom
+    });
+
+    const markerHtml = document.createElement('div');
+    markerHtml.innerHTML = 'Fernando Herrera';
+
+    // const marker = new Marker({
+    //   // color: 'red'
+    //   element: markerHtml
+    // })
+    //   .setLngLat(this.currentLngLat)
+    //   .addTo(this.map);
+
+  }
 }
