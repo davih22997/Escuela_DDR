7752324fe4b681b9f4c22e19345fbaab0eea25ed
davih22997 <davih22997@gmail.com>
2023-10-16 17:52:17 +0200

    feat: Signals in Angular - Handling errors & computed properties

--- 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { UsersServiceService } from '../../services/users-service.service';
 import { User } from '../../interfaces/user-request.interface';
 
@@ -16,6 +16,12 @@ export class UserInfoPageComponent implements OnInit{
   public currentUser = signal<User|undefined>(undefined);
   public userWasFound = signal(true);
 
+  public fullName = computed<string>(() => {
+    if (!this.currentUser() ) return 'Usuario no encontrado';
+
+    return `${this.currentUser()?.first_name } ${this.currentUser()?.last_name}`;
+  });
+
   ngOnInit(): void {
     this.loadUser(this.userId());
   }
@@ -27,9 +33,15 @@ export class UserInfoPageComponent implements OnInit{
     this.currentUser.set(undefined);
 
     this.usersService.getUserById(id)
-      .subscribe(user => {
-        console.log(user);
-        this.currentUser.set(user)
+      .subscribe({
+        next: (user) => {
+          this.currentUser.set(user);
+          this.userWasFound.set(true);
+        },
+        error: () => {
+          this.userWasFound.set(false);
+          this.currentUser.set(undefined);
+        },
       })
 
   }
